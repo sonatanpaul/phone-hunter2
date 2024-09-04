ebe4c7e53714ceaede55763c13cd880191f5b2de
Sonatan Paul <iamsonatanpaul@gmail.com>
2024-09-05 00:04:15 +0600

    phone hunting data display showing task is done

--- script.js
+++ script.js
@@ -7,8 +7,28 @@ const phoneData = async () => {
   displayPhone(phones);
 };
 
-const displayPhone = (phone) => {
-  console.log(phone);
+const displayPhone = (phones) => {
+  const phoneContainer = document.getElementById("phone-container");
+  phones.forEach((phone) => {
+    console.log(phone);
+    const phoneCard = document.createElement("div");
+    phoneCard.classList =
+      "card bg-base-100 w-96 border-2 border-sky-500 border-black shadow-xl";
+    phoneCard.innerHTML = `
+     <figure class="px-10 pt-10">
+              <img
+                src=${phone.image}
+                alt="Shoes"
+                class="rounded-xl"
+              />
+            </figure>
+            <div class="card-body items-center text-center">
+              <h2 class="card-title">${phone.brand}</h2>
+              <p>${phone.phone_name}</p>
+            </div>
+`;
+    phoneContainer.appendChild(phoneCard);
+  });
 };
 
 phoneData();
